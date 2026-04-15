687ebbc22cf3c897c038dd17357e50b65e4d5742
benkoimet@gmail.com <koimettb@gmail.com>
2026-04-15 13:38:26 +0300

    chore: push files for continuation

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,7 +1,16 @@
 package routes
 
 import (
+	// "os"
+	// "strconv"
 	"time"
+
+	// "github.com/bkoimett/url-shortener/api/database"
+	"github.com/bkoimett/url-shortener/api/helpers"
+	"github.com/asaskevich/govalidator"
+	// "github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	// "github.com/google/uuid"
 )
 
 type request struct {
@@ -16,4 +25,38 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+// ShortenURL ...
+func ShortenURL(c *fiber.Ctx) error {
+	// check for the incoming request body
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid URL",
+		})
+	}
+
+	// check for the domain error
+	// users may abuse the shortener by shorting the domain `localhost:3000` itself
+	// leading to a inifite loop, so don't accept the domain for shortening
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "haha... nice try",
+		})
+	}
+
+	// enforce https
+	// all url will be converted to https before storing in database
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+	
 }
