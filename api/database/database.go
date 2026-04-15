f48bb20f1a95aafe0477b423e6b1920eadbd213b
benkoimet@gmail.com <koimettb@gmail.com>
2026-04-15 10:59:17 +0300

    feat: add function to connect to db and allow interaction btwn go and redis

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8" // enables go to interact with redis
+	"os"
+)
+
+var Ctx = context.Background()
+
+func CreateClient(dbNo int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB:       dbNo,
+	})
+	return rdb
+}
